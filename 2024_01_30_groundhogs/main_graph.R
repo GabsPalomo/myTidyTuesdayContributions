249c43c388585b48613a7cfa3ec9832f8b1606e8
GabsPalomo <gabriella.palomo@gmail.com>
2024-02-06 08:57:01 -0500

    Final plot.

--- 2024_01_30_groundhogs/main_graph.R
+++ 2024_01_30_groundhogs/main_graph.R
@@ -46,6 +46,10 @@ yearly_pred <- predictions_groundhogs |>
 # My color palette   
 my_colors <- c('#184e77', '#b58db6' )
 
+# Font used 
+sysfonts::font_add_google("Montserrat", "montserrat")
+showtext::showtext_auto()
+
 # Dumbell plot 
 ggplot(yearly_pred)+
   geom_segment(aes(x = shadow_true, y = year, 
@@ -61,37 +65,58 @@ ggplot(yearly_pred)+
                      limits = c('long winter', 'early spring'), 
                      name = '')+
   scale_y_continuous(limits = c(1955, 2025), breaks = seq(1955, 2025, by = 5))+
-  scale_x_continuous(limits = c(-20, 25), breaks = seq(-20, 25, by=5))+
+  scale_x_continuous(limits = c(-25, 25), 
+                     breaks = seq(-25, 25, by=5))+
   labs(title = "What did the groundhog saw? ",
        subtitle = stringr::str_wrap("The plot below shows the number of predictions per year that a true groundhog
                             has seen since 1955. The color represents what the majority predicted:
                             'long winter' or 'early spring'. If there is no color then there was a tie.  
                             If a groundhog ‘sees its shadow’: six more weeks of winter, but
                             if a groundhog does not see its shadow: early spring. \n", 
-                           width = 95),
+                           width = 105),
        x = 'Number of predictions',
        y = NULL,
        caption = "\n#TidyTuesday 2024 | data source: https://groundhog-day.com/ | @gabbspalomo") +
   geom_vline(xintercept = 0, linetype = 'dashed', color = 'gray40')+
   geom_hline(yintercept = 2024, linetype = 'dashed', color = 'gray40')+
   theme_hc()+
-  theme(legend.position = c(0.44, 1),
+  annotate(geom = "curve", 
+           x = -23, 
+           y = 2019, 
+           xend = -20, 
+           yend = 2024, 
+           linewidth = 0.8,
+           colour = 'gray60',
+           curvature = 0.3,
+           arrow = arrow(length = unit(1.5, "mm"), 
+                         type = "closed"))+
+  annotate('text',
+           x = -25,
+           y = 2019,
+           label = '2024',
+           family = 'montserrat',
+           color = 'gray30',
+           size = 8) +
+  theme(legend.position = c(0.5, 1),
         legend.direction = 'horizontal',
         legend.background = element_rect(fill='transparent'),
         plot.margin = margin(0.8, 0.5, 0.5, 0.5, 'cm'), 
-        plot.title = element_text(size = 20,
-                                  colour = "Black",
+        plot.title = element_text(size = 52,
+                                  color = "Black",
+                                  family = 'montserrat',
                                   face = "bold",
                                   margin = unit(c(0, 0, 0, 0), "cm")),
         plot.title.position = "plot", 
-        plot.subtitle = element_text(size = 12,
-                                     lineheight = 1.1,
+        plot.subtitle = element_text(size = 36,
+                                     lineheight = 0.5,
                                      hjust = 0,
                                      colour = "gray20",
                                      margin = unit(c(0.5, 0, 0.8, 0), "cm")), 
-        axis.text = element_text(size = 12), 
-        axis.title = element_text(size =14), 
-        legend.text = element_text(size = 14)) -> my_plot
+        axis.text = element_text(size = 30), 
+        axis.title = element_text(size =38), 
+        axis.title.x = element_text(margin = margin(t = 0.5, unit = 'cm')), 
+        legend.text = element_text(size = 36), 
+        plot.caption = element_text(size = 30)) -> my_plot
 
 # Export plot 
 ggsave(filename = here::here('2024_01_30_groundhogs', 'plots', 'my_plot.jpg'), 
@@ -100,3 +125,5 @@ ggsave(filename = here::here('2024_01_30_groundhogs', 'plots', 'my_plot.jpg'),
        height = 25, 
        units = 'cm', 
        dpi = 300)  
+
+showtext_end()
